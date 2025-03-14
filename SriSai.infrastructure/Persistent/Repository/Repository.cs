6e707074bcab20f0567ecf8662f43897b4ac20e2
chanakya <chanakya.net@gmail.com>
2025-03-14 14:19:08 +0530

    updated search by id code

--- SriSai.infrastructure/Persistent/Repository/Repository.cs
+++ SriSai.infrastructure/Persistent/Repository/Repository.cs
@@ -1,48 +1,51 @@
-using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using SriSai.Application.interfaces.Reposerty;
 using SriSai.Domain.Entity.Base;
 using SriSai.infrastructure.Persistent.DbContext;
+using System.Linq.Expressions;
 
-namespace SriSai.Infrastructure.Persistent.Repository;
-
-public class Repository<T> : IRepository<T> where T : EntityBase
+namespace SriSai.Infrastructure.Persistent.Repository
 {
-    private readonly SriSaiDbContext _context;
-
-    public Repository(SriSaiDbContext context)
+    public class Repository<T> : IRepository<T> where T : EntityBase
     {
-        _context = context;
-    }
+        private readonly SriSaiDbContext _context;
 
-    public async Task<T?> GetByIdAsync(Guid id)
-    {
-        return await _context.Set<T>().FindAsync(id);
-    }
+        public Repository(SriSaiDbContext context)
+        {
+            _context = context;
+        }
 
-    public async Task<IEnumerable<T>> ListAllAsync()
-    {
-        return await _context.Set<T>().AsNoTracking().ToListAsync();
-    }
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
 
-    public async Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate)
-    {
-        return await _context.Set<T>().AsNoTracking().Where(predicate).ToListAsync();
-    }
+        public async Task<IEnumerable<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
 
-    public async Task<T> AddAsync(T entity)
-    {
-        await _context.Set<T>().AddAsync(entity);
-        return entity;
-    }
+        public async Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AsNoTracking().Where(predicate).ToListAsync();
+        }
 
-    public async Task UpdateAsync(T entity)
-    {
-        _context.Set<T>().Update(entity);
-    }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
 
-    public async Task DeleteAsync(T entity)
-    {
-        _context.Set<T>().Remove(entity);
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
     }
 }
