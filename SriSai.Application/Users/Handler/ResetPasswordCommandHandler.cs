16bd8848509c2a9f05ad6a77db7dc68a8eb3c00f
chanakya <chanakya.net@gmail.com>
2025-04-02 17:17:30 +0530

    save update user before commit

--- SriSai.Application/Users/Handler/ResetPasswordCommandHandler.cs
+++ SriSai.Application/Users/Handler/ResetPasswordCommandHandler.cs
@@ -1,24 +1,24 @@
 using ErrorOr;
 using MediatR;
+using SriSai.Application.Configuration;
 using SriSai.Application.Interfaces.Encryption;
 using SriSai.Application.interfaces.Reposerty;
+using SriSai.Application.Messaging.Command;
 using SriSai.Application.Users.Command;
 using SriSai.Domain.Entity.Users;
 using SriSai.Domain.Errors;
 using SriSai.Domain.Interface;
 using System.Security.Cryptography;
 using System.Text;
-using SriSai.Application.Messaging.Command;
-using SriSai.Application.Configuration;
 
 namespace SriSai.Application.Users.Handler
 {
     public class ResetPasswordCommandHandler : IRequestHandler<ResetPasswordCommand, ErrorOr<string>>
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IHashPassword _passwordHasher;
         private readonly IDateTimeProvider _dateTimeProvider;
         private readonly IMediator _mediator;
+        private readonly IHashPassword _passwordHasher;
+        private readonly IUnitOfWork _unitOfWork;
 
         public ResetPasswordCommandHandler(
             IUnitOfWork unitOfWork,
@@ -44,37 +44,38 @@ namespace SriSai.Application.Users.Handler
 
             // Generate a new random password
             string newPassword = GenerateRandomPassword();
-            
+
             // Hash the new password
             string hashedPassword = _passwordHasher.HashPassword(newPassword);
-            
+
             // Update the user's password
             user.UpdatePassword(hashedPassword);
             user.UpdatedDateTime = _dateTimeProvider.GetUtcNow();
-            
+
+            await _unitOfWork.Repository<UserEntity>().UpdateAsync(user);
             // Save changes
             await _unitOfWork.SaveChangesAsync();
-            
-             SendMessageCommand createUserMessage = new()
-                {
-                    MobileNumber = request.Mobile,
-                    MessageBody =
-                    [
-                        user.FirstName + ' ' + user.LastName, request.Mobile,
-                        newPassword
-                    ],
-                    TemplateName = WhatsAppTemplateNames.UserCreatedTemplate
-                };
-                ErrorOr<Unit> sendMessageResult = await _mediator.Send(createUserMessage, cancellationToken);
+
+            SendMessageCommand createUserMessage = new()
+            {
+                MobileNumber = request.Mobile,
+                MessageBody =
+                [
+                    user.FirstName + ' ' + user.LastName, request.Mobile,
+                    newPassword
+                ],
+                TemplateName = WhatsAppTemplateNames.UserCreatedTemplate
+            };
+            ErrorOr<Unit> sendMessageResult = await _mediator.Send(createUserMessage, cancellationToken);
             // Return the new password to be sent to the user
             return newPassword;
         }
 
-        private string GenerateRandomPassword(int length = 8)
+        private string GenerateRandomPassword(int length = 6)
         {
             const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-            StringBuilder res = new StringBuilder();
-            using (var rng = RandomNumberGenerator.Create())
+            StringBuilder res = new();
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
             {
                 byte[] uintBuffer = new byte[sizeof(uint)];
 
